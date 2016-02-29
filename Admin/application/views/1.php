b154c48abdc3618be9441dfc2e13a7ece2ec7a64
kiphe19 <kiphe19@gmail.com>
2016-02-29 09:58:17 +0700

    Update Upload video with dropzone

--- Admin/application/views/1.php
+++ Admin/application/views/1.php
@@ -10,89 +10,109 @@
 				</div>
 			</div>
 			<div class="ibox-content">
-				<form id="frmVideo">
+				<!-- <form id="frmVideo">
 					<div class="form-group">
-						<label>Video</label>
-						<input type="file" name="" value="" id="files">
+						<label class="btn btn-success" for="files">
+							<input type="file" name="" id="files" multiple style="display: none;" />
+							<i class="fa fa-plus-circle"></i> Select Files..
+						</label>
+						<div style="width: 75%" aria-valuemax="100" aria-valuemin="0" aria-valuenow="75" role="progressbar" class="progress-bar progress-bar-danger">
+							<span class="sr-only">40% Complete (success)</span>
+						</div>
 					</div>
-					<button type="submit" class="btn btn-flat btn-info"><i class="fa fa-send"></i> Publish</button>
-				</form>
-			</div>
-		</div>
-	</div>
-	<div class="col-lg-6">
-		<div class="ibox float-e-margins">
-			<div class="ibox-title">
-				<h5>List <?php echo $title ?></h5>
-				<div class="ibox-tools">
-					<a class="collapse-link">
-						<i class="fa fa-chevron-up"></i>
-					</a>
+					<button type="submit" class="btn btn-flat btn-info"><i class="fa fa-upload"></i> Upload</button>
+				</form> -->
+				<form id="my-awesome-dropzone" class="dropzone dz-clickable" action="#">
+					<div class="dropzone-previews"></div>
+					<button type="submit" class="btn btn-primary pull-right">Submit this form!</button>
+					<div class="dz-default dz-message"><span>Drop files here to upload</span></div></form>
 				</div>
 			</div>
-			<div class="ibox-content">
-				<div class="table-responsive">
-					<table class="table table-bordered">
-						<thead>
-							<tr>
-								<th>Video</th>
-								<th>Position</th>
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php foreach ($content as $key): ?>
+		</div>
+		<div class="col-lg-6">
+			<div class="ibox float-e-margins">
+				<div class="ibox-title">
+					<h5>List <?php echo $title ?></h5>
+					<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+					</div>
+				</div>
+				<div class="ibox-content">
+					<div class="table-responsive">
+						<table class="table table-bordered">
+							<thead>
 								<tr>
-									<td><a href=""><?php echo $key->content ?></a></td>
-									<td>
-										<button type="button" class="btn btn-flat btn-outline btn-success"><i class="fa fa-arrow-up"></i></button>
-										<button type="button" class="btn btn-flat btn-outline btn-success"><i class="fa fa-arrow-down"></i></button>
-									</td>
-									<td>
-										<a href="#" class="btn btn-flat btn-outline btn-primary"><i class="fa fa-eye"></i></a>
-										<a href="#" class="btn btn-outline btn-danger"><i class="fa fa-trash"></i> Delete</a>
-									</td>
+									<th>Video</th>
+									<th>Position</th>
+									<th>Action</th>
 								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($content as $key): ?>
+									<tr>
+										<td><a href=""><?php echo $key->content ?></a></td>
+										<td>
+											<button type="button" class="btn btn-flat btn-outline btn-success"><i class="fa fa-arrow-up"></i></button>
+											<button type="button" class="btn btn-flat btn-outline btn-success"><i class="fa fa-arrow-down"></i></button>
+										</td>
+										<td>
+											<a href="#" class="btn btn-flat btn-outline btn-primary"><i class="fa fa-eye"></i></a>
+											<a href="#" class="btn btn-outline btn-danger"><i class="fa fa-trash"></i> Delete</a>
+										</td>
+									</tr>
 
-							<?php endforeach ?>
-						</tbody>
-					</table>
+								<?php endforeach ?>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/dropzone.js') ?>"></script>
+	<script type="text/javascript">
+		Dropzone.options.myAwesomeDropzone = {
+			url: '<?php echo base_url('ctb/upload'); ?>',
+			paramName: 'files',
+			maxFilesize: 1000,
+			parallelUploads: 5,
+			autoProcessQueue: false,
+			acceptedFiles: 'video/*',
+			
+			init: function(){
+				var a = this;
+				this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
+					e.preventDefault();
+					e.stopPropagation();
+					a.processQueue();
+				});
+			}
+		};
+		$("#frmVideo").submit(function(e){
+			e.preventDefault();
 
-<script type="text/javascript">
-	$("form").submit(function(e){
-		e.preventDefault();
-
-		var form = new FormData(document.getElementById('frmVideo'));
-		var file = document.getElementById('files').files[0];
-		if (file) {
-			form.append('files', file);
-		}
-		$.ajax({
-			url: "../chapters/create",
-			type: 'POST',
-			data: form,             
-			cache: false,
+			var form = new FormData(document.getElementById('frmVideo'));
+			var file = document.getElementById('files').files[0];
+			console.log(file);
+			if (file) {
+				form.append('files', file);
+			}
+			$.ajax({
+				url: "<?php echo base_url('ctb/upload') ?>",
+				type: 'POST',
+				data: form,             
+				cache: false,
         	contentType: false, //must, tell jQuery not to process the data
         	processData: false, //must, tell jQuery not to set contentType
         	success: function(data) {
-	        	console.log(data);
-    	    },
-        	complete: function(XMLHttpRequest) {
-        		var data = XMLHttpRequest.responseText;
         		console.log(data);
         	},
-        	error: function() {
-        		alert("ERROR");
+        	uploadProgress: function (event, position, total, percentComplete){	
+        		$("#progress-bar").width(percentComplete + '%');
+        		$("#progress-bar").html('<div id="progress-status">' + percentComplete +' %</div>')
         	}
-    		}).done(function() { 
-    			console.log('Done');
-		    }).fail(function() {       
-    			alert("fail!");
-    		});
-	})
-</script>
+        })
+		})
+	</script>
